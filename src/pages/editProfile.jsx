6961729c60d5e17b3e2ce9c5f8c42b4220e05b15
unknown <jahnavireddy1709@gmail.com>
2024-09-28 14:04:01 +0530

    initial commit

---
+++ src/pages/editProfile.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+
+export default function EditProfile() {
+  return (
+    <div>
+      <Navbar />
+      <h2>edit profile</h2>
+    </div>
+  )
+}
